396454919de7ba0d546e02c091ab741e46819b85
Valerio Versace <mail@valerioversace.it>
2018-12-28 03:55:23 +0100

    updating grpcio version, creating placeholder for client libraries directory
    updating PyYAML version to fix compatibility with Python 3.7

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     author_email='info@singularitynet.io',
     description='SingularityNET CLI',
     install_requires=[
-        'grpcio-tools==1.14.1',
+        'grpcio-tools==1.17.1',
         'jsonrpcclient==2.5.2',
         'web3==4.2.1',
         'mnemonic==0.18',
